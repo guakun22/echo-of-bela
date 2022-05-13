90b658117304ea378b7d7cbb824a88bf9676d952
guakun22 <guakun22@outlook.com>
2022-05-13 15:26:33 +0800

    OK,  我们可以初始化一个 Bela 了。

--- src/main/java/com/github/guakun22/EchoOfBela.java
+++ src/main/java/com/github/guakun22/EchoOfBela.java
@@ -1,8 +1,26 @@
-package src.main.java.com.github.guakun22;
+package com.github.guakun22;
 
 /**
  * 哈利波特中的贝拉，每个贝拉将模拟一个简陋的 JVM
  */
 public class EchoOfBela {
 
+    private String mainClass;
+
+    private String[] classPathEntries;
+
+    /**
+     * 贝拉：一个迷你 JVM，使用指定的 classpath 和 main class
+     *
+     * @param mainClass 主类的全限定类名
+     *
+     * @param classPathEntries 启动时的 classpath 使用 {@link java.io.File#pathSeparator} 分割，支持文件夹
+     */
+    public EchoOfBela(String mainClass, String[] classPathEntries) {
+        this.mainClass = mainClass;
+        this.classPathEntries = classPathEntries;
+    }
+
+    public static void main(String[] args) {
+    }
 }
