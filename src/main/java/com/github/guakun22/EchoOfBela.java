51e79aaf8f79923abdc040ff9f619ec7a5d922ec
guakun22 <guakun22@outlook.com>
2022-05-13 14:49:46 +0800

    initial

---
+++ src/main/java/com/github/guakun22/EchoOfBela.java
@@ -0,0 +1,8 @@
+package src.main.java.com.github.guakun22;
+
+/**
+ * 哈利波特中的贝拉，每个贝拉将模拟一个简陋的 JVM
+ */
+public class EchoOfBela {
+
+}
---
+++ src/main/java/com/github/guakun22/SampleClass.java
@@ -0,0 +1,11 @@
+package src.main.java.com.github.guakun22;
+
+public class SampleClass {
+    public static void main(String[] args) {
+        System.out.println(foo());
+    }
+
+    private static int foo() {
+        return 2022;
+    }
+}
